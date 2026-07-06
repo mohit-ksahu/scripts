15b101e2ec761000729e1ccb1e0c6b4444c39818
Mohit <214406897+mohit-ksahu@users.noreply.github.com>
2026-07-06 11:46:18 +0530

    Refactor LlamaModel to LLM with updated parameters

--- AIModels/models.py
+++ AIModels/models.py
@@ -2,18 +2,24 @@ import ctypes
 import llama_cpp
 from llama_cpp import Llama
 
-# Suppress llama_cpp verbose internal C/C++ logging
-LOG_CALLBACK = ctypes.CFUNCTYPE(None, ctypes.c_int, ctypes.c_char_p, ctypes.c_void_p)(lambda *_: None)
+LOG_CALLBACK = ctypes.CFUNCTYPE(
+    None,
+    ctypes.c_int,
+    ctypes.c_char_p,
+    ctypes.c_void_p
+)(lambda *_: None)
 llama_cpp.llama_log_set(LOG_CALLBACK, ctypes.c_void_p())
 
-class LlamaModel:
+
+class LLM:
     def __init__(
         self,
         model_path,
-        n_ctx=16384,
-        n_gpu_layers=-1,
+        n_ctx=4096,
+        n_gpu_layers=10,
         flash_attn=True,
         verbose=False,
+        chat_format=None,
         **kwargs
     ):
         self.llm = Llama(
@@ -22,13 +28,16 @@ class LlamaModel:
             n_gpu_layers=n_gpu_layers,
             flash_attn=flash_attn,
             verbose=verbose,
+            chat_format=chat_format,
             **kwargs
         )
 
     def generate(self, messages, stream=False, **kwargs):
         res = self.llm.create_chat_completion(
-            messages=messages, 
-            stream=stream, 
+            messages=messages,
+            stream=stream,
             **kwargs
         )
-        return res if stream else res['choices'][0]['message']['content']
+        if stream:
+            return res
+        return res["choices"][0]["message"]["content"]
